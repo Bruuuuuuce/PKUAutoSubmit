3fbfd54c1102923f05629fe19cd8f49bce8cddb6
you.siki@outlook.com <you.siki@outlook.com>
2020-10-10 20:01:47 +0800

    update

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+/usr/local/envs/workenv/bin/python main.py \
+    -u $1 \
+    -p $2 \
+    --campus $3 \
+    --reason $4 \
+    --destination $5 \
+    --track $6 \
+    --habitation $7 \
+    --district $8 \
+    --street $9
