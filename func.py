a6b397dd22594823bc231508515f1474cdd02240
xiaotianxt <1241811980@qq.com>
2020-10-19 10:31:46 +0800

    fix url parsingbug

--- func.py
+++ func.py
@@ -205,7 +205,7 @@ def fill_in(driver, campus, reason, habitation, district, street):
     print('入校备案填报完毕！')
 
 def wechat_notification(userName, sckey):
-    with request.urlopen(quote('https://sc.ftqq.com/'+sckey+'.send?text=成功报备&desp=学号'+str(userName)+'成功报备', safe='/:?=')) as response:
+    with request.urlopen(quote('https://sc.ftqq.com/'+sckey+'.send?text=成功报备&desp=学号'+str(userName)+'成功报备', safe='/:?=&')) as response:
         response = json.loads(response.read().decode('utf-8'))
     if response['errmsg'] == 'success':
         print('微信通知成功！')
