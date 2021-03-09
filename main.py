66e99f952ade51fa13bd86b76759055f515cbc12
Bruce Ding <48918621+Bruuuuuuce@users.noreply.github.com>
2021-03-10 07:46:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from configparser import ConfigParser
 from func import *
 import warnings
 import sys
+import os
+import re
 warnings.filterwarnings('ignore')
 
 
@@ -19,9 +21,9 @@ def sys_path():
         raise Exception('暂不支持该系统')
 
 
-if __name__ == '__main__':
+def go(config):
     conf = ConfigParser()
-    conf.read('config.ini', encoding='utf8')
+    conf.read(config, encoding='utf8')
 
     userName, password = dict(conf['login']).values()
     campus, reason = dict(conf['common']).values()
@@ -32,9 +34,30 @@ if __name__ == '__main__':
     wechat = conf.getboolean('wechat', '是否需要微信通知')
     sckey = conf['wechat']['SCKEY']
 
-    print('Driver Launching...')
-
-    driver_pjs = webdriver.PhantomJS(executable_path=sys_path(), service_args=['--ignore-ssl-errors=true','--ssl-protocol=TLSv1'])
     run(driver_pjs, userName, password, campus, reason, destination, track,
         habitation, district, street, capture, path, wechat, sckey)
+
+
+if __name__ == '__main__':
+    print('Driver Launching...')
+    driver_pjs = webdriver.PhantomJS(
+        executable_path=sys_path(),
+        service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+    print('Driver Launched\n')
+
+    lst_conf = sorted([
+        fileName for fileName in os.listdir()
+        if re.match(r'^config[0-9][0-9]*\.ini$', fileName)
+    ],
+        key=lambda x: int(re.findall(r'[0-9]+', x)[0]))
+
+    print(f'获取到{len(lst_conf)+1}个学生信息\n')
+    print('||第1个学生备案||')
+    go('config.ini')
+
+    if lst_conf:
+        for num, config in enumerate(lst_conf):
+            print(f'||第{num+2}个学生备案||')
+            go(config)
+
     driver_pjs.quit()
