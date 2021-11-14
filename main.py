565fcf0787c13056074f957910ce4d9225b382d8
sakura <brynhild@pku.edu.cn>
2021-11-14 11:45:17 +0800

    修改了11.14日系统变化产生的bug，主要是出入校事由选项。

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def go(config):
     conf.read(config, encoding='utf8')
 
     userName, password = dict(conf['login']).values()
-    campus, reason = dict(conf['common']).values()
+    campus, reason, detail = dict(conf['common']).values()
     destination, track = dict(conf['out']).values()
     habitation, district, street = dict(conf['in']).values()
     capture = conf.getboolean('capture', '是否需要备案历史截图')
@@ -35,7 +35,7 @@ def go(config):
     wechat = conf.getboolean('wechat', '是否需要微信通知')
     sckey = conf['wechat']['SCKEY']
 
-    run(driver_pjs, userName, password, campus, reason, destination, track,
+    run(driver_pjs, userName, password, campus, reason, detail, destination, track,
         habitation, district, street, capture, path, wechat, sckey)
 
 
